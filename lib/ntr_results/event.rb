8aa37b69958ab45318f527c1d9582f5a88cc08aa
thesilentc <thesilentc@me.com>
2017-05-01 00:31:37 -0400

    testing cli for index selection

--- lib/ntr_results/event.rb
+++ lib/ntr_results/event.rb
@@ -70,18 +70,4 @@ def self.scrape_ntr3
   event
 end
 
-# def self.scrape_index_page(index_url)
-#     index_page = Nokogiri::HTML(open(index_url))
-#     students = []
-#     index_page.css("div.roster-cards-container").each do |card|
-#       card.css(".student-card a").each do |student|
-#         student_profile_link = "./fixtures/student-site/#{student.attr('href')}"
-#         student_location = student.css('.student-location').text
-#         student_name = student.css('.student-name').text
-#         students << {name: student_name, location: student_location, profile_url: student_profile_link}
-#       end
-#     end
-#     students
-#   end
-
 end
