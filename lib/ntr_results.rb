fa7ec164c14f12ec439fe4a14e754539fd8c2201
thesilentc <thesilentc@me.com>
2017-05-18 21:50:12 -0400

    cleaned up gem

--- lib/ntr_results.rb
+++ lib/ntr_results.rb
@@ -7,5 +7,4 @@ require 'open-uri'
 require_relative "./ntr_results/version"
 require_relative './ntr_results/scraper'
 require_relative './ntr_results/cli'
-
 require_relative './ntr_results/event'  
