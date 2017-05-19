ec157340bc3d1a00992f443cd03cc24c804fe84e
thesilentc <thesilentc@me.com>
2017-05-18 21:46:23 -0400

    Refactored page scraper

--- lib/ntr_results/scraper.rb
+++ lib/ntr_results/scraper.rb
@@ -1,25 +1,25 @@
 class NtrResults::Scraper
-  
+
 
   def self.scrape_events
 
-    self.scrape_ntr1
-    self.scrape_ntr2
-    self.scrape_ntr3
-    # self.scrape_ntr
+    # self.scrape_ntr1
+    # self.scrape_ntr2
+    # self.scrape_ntr3
+    self.scrape_ntr
 
-    
+
   end
 
   # remove comment below to scrape all events from one page and print to array
 
-  # def self.scrape_ntr
-  #     doc = Nokogiri::HTML(open("http://nationalteamroping.com/articles.sec-26-1-results.html"))
-  #     event = self.new
-  #     event.name = doc.css(".title-txt h1 a").collect{|e| e.text}
-  #     
-  #     event
-  # end
+  def self.scrape_ntr
+      doc = Nokogiri::HTML(open("http://nationalteamroping.com/articles.sec-26-1-results.html"))
+      event = NtrResults::Event.new
+      event.name = doc.css(".title-txt h1 a").collect{|e| e.text}
+
+      # event
+  end
 
 
   def self.scrape_ntr1
