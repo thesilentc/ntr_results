d08f5b629d371fb217208117762ddcbf5e9b7d0b
thesilentc <thesilentc@me.com>
2017-05-23 20:49:27 -0400

    set up new scraper filer

---
+++ lib/ntr_results/testscraper.rb
@@ -0,0 +1,7 @@
+# def self.scrape_ntr
+    doc = Nokogiri::HTML(open("http://nationalteamroping.com/articles.sec-26-1-results.html"))
+    event = NtrResults::Event.new
+    event.name = doc.css(".title-txt h1 a").collect{|e| e.text}
+
+    # event
+end
